e68abd722a225204efff4631701144d2acec215c
962554 <254557023+962554@users.noreply.github.com>
2026-03-04 16:56:24 +0000

    chore: apply fixes from golangci-lint

--- get_content.go
+++ get_content.go
@@ -16,12 +16,16 @@ func getHeadingFromHTML(html string) string {
 	if !strings.Contains(html, H1) && !strings.Contains(html, H2) {
 		return ""
 	}
+
 	buf := strings.NewReader(html)
+
 	doc, err := goquery.NewDocumentFromReader(buf)
 	if err != nil {
 		return ""
 	}
+
 	h1 := doc.Find("h1, h2")
+
 	return h1.First().Text()
 }
 
@@ -29,18 +33,22 @@ func getFirstParagraphFromHTML(html string) string {
 	if !strings.Contains(html, P) {
 		return ""
 	}
+
 	buf := strings.NewReader(html)
+
 	doc, err := goquery.NewDocumentFromReader(buf)
 	if err != nil {
 		return ""
 	}
 
 	main := doc.Find("main")
+
 	var p string
 	if main.Length() > 0 {
 		p = main.Find("p").First().Text()
 	} else {
 		p = doc.Find("p").First().Text()
 	}
+
 	return p
 }
